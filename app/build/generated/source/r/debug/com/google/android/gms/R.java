4c2679a20afd51c458f8ab8403fd86768d7983f8
lucascviveiros <lucasdicavalcanti@hotmail.com>
2017-09-23 16:01:13 +0000

    Login Face

--- app/build/generated/source/r/debug/com/google/android/gms/R.java
+++ app/build/generated/source/r/debug/com/google/android/gms/R.java
@@ -8,64 +8,65 @@ package com.google.android.gms;
 
 public final class R {
     public static final class attr {
-        public static final int buttonSize = 0x7f010130;
-        public static final int circleCrop = 0x7f010105;
-        public static final int colorScheme = 0x7f010131;
-        public static final int imageAspectRatio = 0x7f010104;
-        public static final int imageAspectRatioAdjust = 0x7f010103;
-        public static final int scopeUris = 0x7f010132;
+        public static final int buttonSize = 0x7f01013b;
+        public static final int circleCrop = 0x7f010110;
+        public static final int colorScheme = 0x7f01013c;
+        public static final int imageAspectRatio = 0x7f01010f;
+        public static final int imageAspectRatioAdjust = 0x7f01010e;
+        public static final int scopeUris = 0x7f01013d;
     }
     public static final class color {
-        public static final int common_google_signin_btn_text_dark = 0x7f0c0066;
-        public static final int common_google_signin_btn_text_dark_default = 0x7f0c0016;
-        public static final int common_google_signin_btn_text_dark_disabled = 0x7f0c0017;
-        public static final int common_google_signin_btn_text_dark_focused = 0x7f0c0018;
-        public static final int common_google_signin_btn_text_dark_pressed = 0x7f0c0019;
-        public static final int common_google_signin_btn_text_light = 0x7f0c0067;
-        public static final int common_google_signin_btn_text_light_default = 0x7f0c001a;
-        public static final int common_google_signin_btn_text_light_disabled = 0x7f0c001b;
-        public static final int common_google_signin_btn_text_light_focused = 0x7f0c001c;
-        public static final int common_google_signin_btn_text_light_pressed = 0x7f0c001d;
-        public static final int common_google_signin_btn_tint = 0x7f0c0068;
+        public static final int common_google_signin_btn_text_dark = 0x7f0c0080;
+        public static final int common_google_signin_btn_text_dark_default = 0x7f0c002e;
+        public static final int common_google_signin_btn_text_dark_disabled = 0x7f0c002f;
+        public static final int common_google_signin_btn_text_dark_focused = 0x7f0c0030;
+        public static final int common_google_signin_btn_text_dark_pressed = 0x7f0c0031;
+        public static final int common_google_signin_btn_text_light = 0x7f0c0081;
+        public static final int common_google_signin_btn_text_light_default = 0x7f0c0032;
+        public static final int common_google_signin_btn_text_light_disabled = 0x7f0c0033;
+        public static final int common_google_signin_btn_text_light_focused = 0x7f0c0034;
+        public static final int common_google_signin_btn_text_light_pressed = 0x7f0c0035;
+        public static final int common_google_signin_btn_tint = 0x7f0c0082;
     }
     public static final class drawable {
-        public static final int common_full_open_on_phone = 0x7f020055;
-        public static final int common_google_signin_btn_icon_dark = 0x7f020056;
-        public static final int common_google_signin_btn_icon_dark_focused = 0x7f020057;
-        public static final int common_google_signin_btn_icon_dark_normal = 0x7f020058;
-        public static final int common_google_signin_btn_icon_dark_normal_background = 0x7f020059;
-        public static final int common_google_signin_btn_icon_disabled = 0x7f02005a;
-        public static final int common_google_signin_btn_icon_light = 0x7f02005b;
-        public static final int common_google_signin_btn_icon_light_focused = 0x7f02005c;
-        public static final int common_google_signin_btn_icon_light_normal = 0x7f02005d;
-        public static final int common_google_signin_btn_icon_light_normal_background = 0x7f02005e;
-        public static final int common_google_signin_btn_text_dark = 0x7f02005f;
-        public static final int common_google_signin_btn_text_dark_focused = 0x7f020060;
-        public static final int common_google_signin_btn_text_dark_normal = 0x7f020061;
-        public static final int common_google_signin_btn_text_dark_normal_background = 0x7f020062;
-        public static final int common_google_signin_btn_text_disabled = 0x7f020063;
-        public static final int common_google_signin_btn_text_light = 0x7f020064;
-        public static final int common_google_signin_btn_text_light_focused = 0x7f020065;
-        public static final int common_google_signin_btn_text_light_normal = 0x7f020066;
-        public static final int common_google_signin_btn_text_light_normal_background = 0x7f020067;
-        public static final int googleg_disabled_color_18 = 0x7f02006e;
-        public static final int googleg_standard_color_18 = 0x7f02006f;
+        public static final int common_full_open_on_phone = 0x7f020071;
+        public static final int common_google_signin_btn_icon_dark = 0x7f020072;
+        public static final int common_google_signin_btn_icon_dark_focused = 0x7f020073;
+        public static final int common_google_signin_btn_icon_dark_normal = 0x7f020074;
+        public static final int common_google_signin_btn_icon_dark_normal_background = 0x7f020075;
+        public static final int common_google_signin_btn_icon_disabled = 0x7f020076;
+        public static final int common_google_signin_btn_icon_light = 0x7f020077;
+        public static final int common_google_signin_btn_icon_light_focused = 0x7f020078;
+        public static final int common_google_signin_btn_icon_light_normal = 0x7f020079;
+        public static final int common_google_signin_btn_icon_light_normal_background = 0x7f02007a;
+        public static final int common_google_signin_btn_text_dark = 0x7f02007b;
+        public static final int common_google_signin_btn_text_dark_focused = 0x7f02007c;
+        public static final int common_google_signin_btn_text_dark_normal = 0x7f02007d;
+        public static final int common_google_signin_btn_text_dark_normal_background = 0x7f02007e;
+        public static final int common_google_signin_btn_text_disabled = 0x7f02007f;
+        public static final int common_google_signin_btn_text_light = 0x7f020080;
+        public static final int common_google_signin_btn_text_light_focused = 0x7f020081;
+        public static final int common_google_signin_btn_text_light_normal = 0x7f020082;
+        public static final int common_google_signin_btn_text_light_normal_background = 0x7f020083;
+        public static final int googleg_disabled_color_18 = 0x7f02008a;
+        public static final int googleg_standard_color_18 = 0x7f02008b;
     }
     public static final class id {
         public static final int adjust_height = 0x7f0d0042;
         public static final int adjust_width = 0x7f0d0043;
         public static final int auto = 0x7f0d002e;
+        public static final int button = 0x7f0d0054;
         public static final int center = 0x7f0d0030;
         public static final int dark = 0x7f0d004c;
         public static final int icon_only = 0x7f0d0049;
         public static final int light = 0x7f0d004d;
         public static final int none = 0x7f0d0018;
         public static final int normal = 0x7f0d001a;
-        public static final int radio = 0x7f0d006c;
+        public static final int radio = 0x7f0d0077;
         public static final int standard = 0x7f0d004a;
-        public static final int text = 0x7f0d00ad;
-        public static final int text2 = 0x7f0d00ab;
-        public static final int toolbar = 0x7f0d0086;
+        public static final int text = 0x7f0d00c7;
+        public static final int text2 = 0x7f0d00c5;
+        public static final int toolbar = 0x7f0d0092;
         public static final int wide = 0x7f0d004b;
         public static final int wrap_content = 0x7f0d002d;
     }
@@ -73,30 +74,30 @@ public final class R {
         public static final int google_play_services_version = 0x7f0b0006;
     }
     public static final class string {
-        public static final int common_google_play_services_enable_button = 0x7f070013;
-        public static final int common_google_play_services_enable_text = 0x7f070014;
-        public static final int common_google_play_services_enable_title = 0x7f070015;
-        public static final int common_google_play_services_install_button = 0x7f070016;
-        public static final int common_google_play_services_install_text = 0x7f070017;
-        public static final int common_google_play_services_install_title = 0x7f070018;
-        public static final int common_google_play_services_notification_ticker = 0x7f070019;
-        public static final int common_google_play_services_unknown_issue = 0x7f07001a;
-        public static final int common_google_play_services_unsupported_text = 0x7f07001b;
-        public static final int common_google_play_services_update_button = 0x7f07001c;
-        public static final int common_google_play_services_update_text = 0x7f07001d;
-        public static final int common_google_play_services_update_title = 0x7f07001e;
-        public static final int common_google_play_services_updating_text = 0x7f07001f;
-        public static final int common_google_play_services_wear_update_text = 0x7f070020;
-        public static final int common_open_on_phone = 0x7f070021;
-        public static final int common_signin_button_text = 0x7f070022;
-        public static final int common_signin_button_text_long = 0x7f070023;
+        public static final int common_google_play_services_enable_button = 0x7f07002a;
+        public static final int common_google_play_services_enable_text = 0x7f07002b;
+        public static final int common_google_play_services_enable_title = 0x7f07002c;
+        public static final int common_google_play_services_install_button = 0x7f07002d;
+        public static final int common_google_play_services_install_text = 0x7f07002e;
+        public static final int common_google_play_services_install_title = 0x7f07002f;
+        public static final int common_google_play_services_notification_ticker = 0x7f070030;
+        public static final int common_google_play_services_unknown_issue = 0x7f070031;
+        public static final int common_google_play_services_unsupported_text = 0x7f070032;
+        public static final int common_google_play_services_update_button = 0x7f070033;
+        public static final int common_google_play_services_update_text = 0x7f070034;
+        public static final int common_google_play_services_update_title = 0x7f070035;
+        public static final int common_google_play_services_updating_text = 0x7f070036;
+        public static final int common_google_play_services_wear_update_text = 0x7f070037;
+        public static final int common_open_on_phone = 0x7f070038;
+        public static final int common_signin_button_text = 0x7f070039;
+        public static final int common_signin_button_text_long = 0x7f07003a;
     }
     public static final class styleable {
-        public static final int[] LoadingImageView = { 0x7f010103, 0x7f010104, 0x7f010105 };
+        public static final int[] LoadingImageView = { 0x7f01010e, 0x7f01010f, 0x7f010110 };
         public static final int LoadingImageView_circleCrop = 2;
         public static final int LoadingImageView_imageAspectRatio = 1;
         public static final int LoadingImageView_imageAspectRatioAdjust = 0;
-        public static final int[] SignInButton = { 0x7f010130, 0x7f010131, 0x7f010132 };
+        public static final int[] SignInButton = { 0x7f01013b, 0x7f01013c, 0x7f01013d };
         public static final int SignInButton_buttonSize = 0;
         public static final int SignInButton_colorScheme = 1;
         public static final int SignInButton_scopeUris = 2;
--- app/src/main/java/com/example/kazz/quick_chat_project/LoginActivity.java
+++ app/src/main/java/com/example/kazz/quick_chat_project/LoginActivity.java
@@ -1,21 +1,27 @@
 package com.example.kazz.quick_chat_project;
 
-import android.content.Context;
-
 import android.content.Intent;
+import android.nfc.Tag;
 import android.os.Bundle;
-import android.os.PersistableBundle;
 import android.support.annotation.NonNull;
-import android.support.annotation.Nullable;
-import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
-import android.util.AttributeSet;
 import android.util.Log;
-import android.view.LayoutInflater;
 import android.view.View;
-import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.Toast;
 
+import com.facebook.AccessToken;
+import com.facebook.AccessTokenTracker;
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.FacebookSdk;
+import com.facebook.Profile;
+import com.facebook.ProfileTracker;
+import com.facebook.appevents.AppEventsLogger;
+import com.facebook.login.LoginManager;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
 import com.google.android.gms.auth.api.Auth;
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
@@ -27,6 +33,7 @@ import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthCredential;
 import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FacebookAuthProvider;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.auth.GoogleAuthProvider;
@@ -38,15 +45,20 @@ import com.google.firebase.auth.GoogleAuthProvider;
 public class LoginActivity extends AppCompatActivity {
 
     private final static int RC_SIGN_IN = 2;
-    SignInButton button;
     GoogleApiClient mGoogleApiClient;
     private FirebaseAuth mAuth;
     private FirebaseAuth.AuthStateListener mAuthListener;
-
+    private CallbackManager callbackManager;
+    private AccessTokenTracker accessTokenTracker;
+    private ProfileTracker profileTracker;
+    private LoginButton loginButton; //face
+    private SignInButton button; //gmail
+    String xStr;
     @Override
     protected void onStart() {
         super.onStart();
         mAuth.addAuthStateListener(mAuthListener);
+        FirebaseUser currentUser = mAuth.getCurrentUser();
     }
 
     @Override
@@ -60,27 +72,66 @@ public class LoginActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        FacebookSdk.sdkInitialize(getApplicationContext());
         setContentView(R.layout.activity_login);
-
+        AppEventsLogger.activateApp(getApplication());
         mAuth = FirebaseAuth.getInstance();
+        callbackManager = CallbackManager.Factory.create();
+        loginButton = (LoginButton) findViewById(R.id.login_button);
+        loginButton.setReadPermissions("email", "public_profile", "user_birthday");
+        loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+                handleFacebookAccessToken(loginResult.getAccessToken());
+            }
+
+            @Override
+            public void onCancel() {
+                // App code
+            }
+
+            @Override
+            public void onError(FacebookException exception) {
+                // App code
+            }
+        });
+
+        accessTokenTracker = new AccessTokenTracker() {
+            @Override
+            protected void onCurrentAccessTokenChanged(AccessToken oldToken, AccessToken currentToken1) {
+
+            }
+        };
+
+        profileTracker = new ProfileTracker() {
+            @Override
+            protected void onCurrentProfileChanged(Profile profile, Profile profile1) {
+                 //   xStr = profile.getFirstName().toString();
+                }
+        };
+
+        accessTokenTracker.startTracking();
+        profileTracker.startTracking();
+
         mAuthListener = new FirebaseAuth.AuthStateListener() {
             @Override
             public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
                 FirebaseUser user = firebaseAuth.getCurrentUser();
                 if (user != null) {
-                    Toast.makeText(LoginActivity.this, "Logou!", Toast.LENGTH_SHORT).show();
+                    Toast.makeText(LoginActivity.this, "Logou! " , Toast.LENGTH_SHORT).show();
                     startActivity(new Intent(LoginActivity.this, QuickActivity.class));
 
                 } else {
-                    //Toast.makeText(LoginActivity.this, "Nao logou :/!", Toast.LENGTH_SHORT).show();
+                    Toast.makeText(LoginActivity.this, "Nao logou !", Toast.LENGTH_SHORT).show();
                     Log.d("Tag", "onAuthStateChanged:signed_out");
                 }
             }
         };
 
         button = (SignInButton) findViewById(R.id.googleBtn);
+
         GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
-                .requestIdToken(getString(R.string.default_web_client_id))
+                .requestIdToken(getString(R.string.default_web_client_id ))
                 .requestEmail()
                 .build();
 
@@ -100,6 +151,7 @@ public class LoginActivity extends AppCompatActivity {
                 signIn();
             }
         });
+
     }
 
     public void signIn(){
@@ -110,40 +162,45 @@ public class LoginActivity extends AppCompatActivity {
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-
         if(requestCode == RC_SIGN_IN){
             GoogleSignInResult result = Auth.GoogleSignInApi.getSignInResultFromIntent(data);
             if(result.isSuccess()){
                 GoogleSignInAccount account = result.getSignInAccount();
                 firebaseAuthWithGoogle(account);
             }
-
+        }else{
+            callbackManager.onActivityResult(requestCode, resultCode, data);
         }
-
     }
 
-
+    private void handleFacebookAccessToken(AccessToken token) {
+        AuthCredential credential = FacebookAuthProvider.getCredential(token.getToken());
+        mAuth.signInWithCredential(credential)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            FirebaseUser user = mAuth.getCurrentUser();
+                        } else {
+                            Toast.makeText(LoginActivity.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+    }
 
     private void firebaseAuthWithGoogle(GoogleSignInAccount acct) {
-    //    Log.d(TAG, "firebaseAuthWithGoogle:" + acct.getId());
 
         AuthCredential credential = GoogleAuthProvider.getCredential(acct.getIdToken(), null);
         mAuth.signInWithCredential(credential)
                 .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
                     @Override
                     public void onComplete(@NonNull Task<AuthResult> task) {
-      //                  Log.d(TAG, "signInWithCredential:onComplete:" + task.isSuccessful());
-
-                        // If sign in fails, display a message to the user. If sign in succeeds
-                        // the auth state listener will be notified and logic to handle the
-                        // signed in user can be handled in the listener.
                         if (!task.isSuccessful()) {
-        //                    Log.w(TAG, "signInWithCredential", task.getException());
                             Toast.makeText(LoginActivity.this, "Authentication failed.",
                                     Toast.LENGTH_SHORT).show();
                         }
-                        // ...
-                    }
+                     }
                 });
     }
 }
